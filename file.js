0763e00b65fb15ae1107df4aee13ff08463c81da
henimezrani <henimezrani@gmail.com>
2019-11-06 10:20:20 +0100

    firas/05

--- file.js
+++ file.js
@@ -1,35 +1,30 @@
+var object ={}
+ object.shoparray=[];
+ object.name = "Telescope store"
+var total = 0 ;
 $(function(){
 	$(".btn1").on("click",function(){
 		$("ul").append("<li>"+$("#h31").text()+"</li>")
+		object.shoparray.push($("#h31").text())
+		total+=160 
 	})
 	$(".btn2").on("click",function(){
 		$("ul").append("<li>"+$("#h32").text()+"</li>")
+		object.shoparray.push($("#h32").text())
+		total+=120 
 	})
 	$(".btn3").on("click",function(){
 		$("ul").append("<li>"+$("#h33").text()+"</li>")
+		object.shoparray.push($("#h33").text())
+		total+=1000 
 	})
 	$(".btn4").on("click",function(){
 		$("ul").append("<li>"+$("#h34").text()+"</li>")
+		object.shoparray.push($("#h34").text())
+		total+=600  
 	})
 	$(".btn5").on("click",function(){
 		$("ul").append("<li>"+$("#h35").text()+"</li>")
-	})
-	$(".btn6").on("click",function(){
-		$("ul").append("<li>"+$("#h36").text()+"</li>")
-	})
-	$(".btn7").on("click",function(){
-		$("ul").append("<li>"+$("#h37").text()+"</li>")
-	})
-	$(".btn8").on("click",function(){
-		$("ul").append("<li>"+$("#h38").text()+"</li>")
-	})
-	$(".btn9").on("click",function(){
-		$("ul").append("<li>"+$("#h39").text()+"</li>")
-	})
-	$(".btn10").on("click",function(){
-		$("ul").append("<li>"+$("#h3").text()+"</li>")
-	})
-	$(".btn11").on("click",function(){
-		$("ul").append("<li>"+$("#h36").text()+"</li>")
-	})
-})
+		object.shoparray.push($("#h35").text())
+		total+=400
+	
